f24e7217f58f239c53c77fc15191ac4b179b5e52
joservarelaf <varelafernandez@gmx.com>
2020-12-28 16:04:45 +0100

    1

---
+++ index.php
@@ -0,0 +1,52 @@
+<?php
+
+function count_visitors(){
+    $file = 'counter.txt';
+
+    if (file_exists($file)) {
+        $count = file_get_contents($file) + 1;
+        file_put_contents($file, $count);
+
+        return $count;
+    } else {
+        file_put_contents($file, 1);
+        return 1;
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Goldman&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="normalize.css">
+    <link rel="stylesheet" href="style.css">
+    <title>Visitor Counter</title>
+</head>
+<body>
+    <div class="wrap">
+        <h2>Visitor Counter</h2>
+        <h4>Code Example</h4>
+        <div class="visitors">
+            <p class="number">
+                <?php echo count_visitors();?>
+            </p>
+            <p class="text">Visits</p>
+        </div>
+        <p class="how">
+        This counter works with PHP.
+        <br />
+        The number of visits it´s increased when the page is charged or recharged.
+        </p>
+
+        <p class="how">
+	Files avaliable here: <a href="http://">ENLACE AL CODIGO</a>
+        <br />
+        Visit my Github page: <a href="http://">jrcode.github.com</a> 
+        </p>
+    </div>
+</body>
+</html>
